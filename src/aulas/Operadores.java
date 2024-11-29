cde8b9abc7213f9804174e6eaefb271082bad955
monteiro007code <rodrigomonteiro745@yahoo.com>
2024-11-29 17:38:00 -0300

    incremento

---
+++ src/aulas/Operadores.java
@@ -0,0 +1,38 @@
+package aulas;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+        int resultado = 1 + 2;
+        System.out.println(resultado);
+
+        resultado = resultado - 1;
+        System.out.println(resultado);
+
+        resultado = resultado * 2;
+        System.out.println(resultado);
+
+        resultado = resultado / 2;
+        System.out.println(resultado);
+
+        resultado = resultado % 7;
+        System.out.println(resultado);
+
+        String primeiroNome = "Esta é ";
+        String segundoNome = "uma string concatenada";
+        String terceiroNome = primeiroNome + segundoNome;
+        System.out.println(terceiroNome);
+
+        resultado = resultado + 1;
+        System.out.println(resultado);// 3
+
+        resultado++; // adicionando 1
+        System.out.println(resultado);// 4
+
+        System.out.println(resultado++);// 4
+
+        System.out.println(++resultado);// 6
+
+    }
+
+}
