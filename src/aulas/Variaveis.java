bd389dac554d0e9b411fbef89a4ece787b01013c
monteiro007code <rodrigomonteiro745@yahoo.com>
2024-11-29 17:02:17 -0300

    Inicio do curso de java

---
+++ src/aulas/Variaveis.java
@@ -0,0 +1,14 @@
+package aulas;
+
+public class Variaveis {
+    public static void main(String[] args) {
+    
+        //CONVENÇÃO JAVA 
+        int idade = 35;
+        String nome = "Rodrigo";
+        String nomeDoMinhaGatinha = "Lisinha" ;
+
+    
+    }
+
+}
